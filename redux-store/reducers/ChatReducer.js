c3d9d980609b5d932d681a969d2ba652217c7a23
“HyunJi-Clara-Lee” <hyun0036@stud.kea.dk>
2021-06-13 18:53:14 +0200

    'read' attribute updating issue fixed

--- redux-store/reducers/ChatReducer.js
+++ redux-store/reducers/ChatReducer.js
@@ -18,126 +18,196 @@ const initialState = {
   myChatroomMessages: null,
   chatroomsUsersInfo: null,
   newMessage: null,
+  allChatMessages: null,
 };
 
 const ChatReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_CHATROOMS:
-      let chatrooms = [];
+      {
+        let chatrooms = [];
 
-      if (action.payload.data != null) {
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
-        for (const [key, value] of Object.entries(action.payload.data)) {
-          if (
-            value.participants[0] == action.payload.loggedInUserId ||
-            value.participants[1] == action.payload.loggedInUserId
-          ) {
-            chatrooms.push(new ChatRoom(key, value.participants, new Date(value.createdDate), value.messages));
+        if (action.payload.data != null) {
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+          for (const [key, value] of Object.entries(action.payload.data)) {
+            if (
+              value.participants[0] == action.payload.loggedInUserId ||
+              value.participants[1] == action.payload.loggedInUserId
+            ) {
+              chatrooms.push(new ChatRoom(key, value.participants, new Date(value.createdDate), value.messages));
+            }
           }
         }
+
+        return {
+          ...state,
+          myChatrooms: chatrooms,
+        };
       }
 
-      return {
-        ...state,
-        myChatrooms: chatrooms,
-      };
-
     case CREATE_CHATROOM:
-      return {
-        ...state,
-        myChatrooms: [...state.myChatrooms, action.payload.newChatroom],
-        openedNewChatId: [action.payload.systemId, action.payload.newChatroom.id],
-      };
+      {
+        return {
+          ...state,
+          myChatrooms: [...state.myChatrooms, action.payload.newChatroom],
+          openedNewChatId: [action.payload.systemId, action.payload.newChatroom.id],
+        };
+      }
 
     case SEND_MESSAGE:
-      return {
-        ...state,
-        myChatroomMessages: [...state.myChatroomMessages, action.payload],
-        newMessage: null,
-      };
+      {
+        let anyUnreadMessage = false;
+        state.myChatrooms.forEach(chatroom => {
+          if (chatroom.participants[0] == action.payload.loggedInUserId || chatroom.participants[1] == action.payload.loggedInUserId) {
+            state.allChatMessages.forEach(message => {
+              if ((message.chatroomId == chatroom.id) && (message.writtenBy != action.payload.loggedInUser) && message.read == false) {
+                anyUnreadMessage = true;
+              }
+            })
+          }
+        })
 
-    case GET_CHATROOM_MESSAGES:
-      let chatroomMessages = [];
-      let anyUnead = true;
-
-      if (action.payload.data != null) {
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
-        for (const [key, value] of Object.entries(action.payload.data)) {
-          state.myChatrooms.forEach(chatroom => {
-            if (value.chatroomId == chatroom.id) {
-              if (value.writtenBy != action.payload.loggedInUser && !value.read) { anyUnead = false; }
-
-              chatroomMessages.push(
-                new ChatMessage(key, value.chatroomId, value.writtenBy, value.text, new Date(value.createdDate), value.read)
-              );
-            }
-          });
+        if (anyUnreadMessage) {
+          return {
+            ...state,
+            myChatroomMessages: [...state.myChatroomMessages, action.payload.newMessage],
+            newMessage: true,
+          };
+        } else {
+          return {
+            ...state,
+            myChatroomMessages: [...state.myChatroomMessages, action.payload.newMessage],
+            newMessage: null,
+          };
         }
       }
 
-      if (anyUnead) {
-        return {
-          ...state,
-          myChatroomMessages: chatroomMessages,
-          newMessage: null
-        };
-      } else {
-        return {
-          ...state,
-          myChatroomMessages: chatroomMessages,
-          newMessage: true
-        };
+
+    case GET_CHATROOM_MESSAGES:
+      {
+        let chatroomMessages = [];
+        let allChatMessages = [];
+
+        let anyUnread = false;
+
+        if (action.payload.data != null) {
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+          for (const [key, value] of Object.entries(action.payload.data)) {
+            state.myChatrooms.forEach(chatroom => {
+              if (value.chatroomId == chatroom.id) {
+                if (value.writtenBy != action.payload.loggedInUser && !value.read) { anyUnread = true; }
+
+                chatroomMessages.push(
+                  new ChatMessage(key, value.chatroomId, value.writtenBy, value.text, new Date(value.createdDate), value.read)
+                );
+              }
+            });
+          }
+
+          for (const [key, value] of Object.entries(action.payload.data)) {
+            allChatMessages.push(
+              new ChatMessage(key, value.chatroomId, value.writtenBy, value.text, new Date(value.createdDate), value.read)
+            );
+          }
+        }
+
+        console.log(allChatMessages)
+
+        if (anyUnread) {
+          return {
+            ...state,
+            myChatroomMessages: chatroomMessages,
+            allChatMessages: allChatMessages,
+            newMessage: true
+          };
+        } else {
+          return {
+            ...state,
+            myChatroomMessages: chatroomMessages,
+            allChatMessages: allChatMessages,
+            newMessage: null
+          };
+        }
       }
 
     case GET_CHATROOMS_USERS_INFO:
-      let chatroomsUsers = [];
+      {
+        let chatroomsUsers = [];
 
-      if (action.payload.data != null) {
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
-        for (const [key, value] of Object.entries(action.payload.data)) {
-          if (state.myChatrooms !== undefined || state.myChatrooms !== null) {
+        if (action.payload.data != null) {
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+          for (const [key, value] of Object.entries(action.payload.data)) {
+            if (state.myChatrooms !== undefined || state.myChatrooms !== null) {
+              state.myChatrooms.forEach(chatroom => {
+                if (chatroom.participants[0] == action.payload.myId && chatroom.participants[1] == value.id) {
+                  chatroomsUsers.push(new User(value.id, value.name, value.email, value.profile, null, null, null));
+                } else if (chatroom.participants[1] == action.payload.myId && chatroom.participants[0] == value.id) {
+                  chatroomsUsers.push(new User(value.id, value.name, value.email, value.profile, null, null, null));
+                }
+              });
+            }
+          }
+        }
+
+        return {
+          ...state,
+          chatroomsUsersInfo: chatroomsUsers,
+        };
+      }
+
+
+
+    case REMOVE_NEW_CHAT_INFO:
+      {
+        return {
+          ...state,
+        };
+      }
+
+
+
+    case SET_CHATROOM_MESSAGES_READ:
+      {
+        chatroomMessages = [];
+
+        if (action.payload != null) {
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+          for (const [key, value] of Object.entries(action.payload.data)) {
             state.myChatrooms.forEach(chatroom => {
-              if (chatroom.participants[0] == action.payload.myId && chatroom.participants[1] == value.id) {
-                chatroomsUsers.push(new User(value.id, value.name, value.email, value.profile, null, null, null));
-              } else if (chatroom.participants[1] == action.payload.myId && chatroom.participants[0] == value.id) {
-                chatroomsUsers.push(new User(value.id, value.name, value.email, value.profile, null, null, null));
+              if (value.chatroomId == chatroom.id) {
+                chatroomMessages.push(
+                  new ChatMessage(key, value.chatroomId, value.writtenBy, value.text, new Date(value.createdDate), value.read)
+                );
               }
             });
           }
         }
-      }
 
-      return {
-        ...state,
-        chatroomsUsersInfo: chatroomsUsers,
-      };
+        let anyUnread = false;
+        state.myChatrooms.forEach(chatroom => {
+          if (chatroom.participants[0] == action.payload.loggedInUserId || chatroom.participants[1] == action.payload.loggedInUserId) {
+            state.allChatMessages.forEach(message => {
+              if (action.payload.chatroomId != message.chatroomId && message.writtenBy != action.payload.loggedInUser && message.read == false) {
+                anyUnread = true;
+              }
+            })
+          }
+        })
 
-    case REMOVE_NEW_CHAT_INFO:
-      return {
-        ...state,
-      };
-
-    case SET_CHATROOM_MESSAGES_READ:
-      chatroomMessages = [];
-
-      if (action.payload != null) {
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
-        for (const [key, value] of Object.entries(action.payload)) {
-          state.myChatrooms.forEach(chatroom => {
-            if (value.chatroomId == chatroom.id) {
-              chatroomMessages.push(
-                new ChatMessage(key, value.chatroomId, value.writtenBy, value.text, new Date(value.createdDate), value.read)
-              );
-            }
-          });
+        if (anyUnread) {
+          return {
+            ...state,
+            myChatroomMessages: chatroomMessages,
+            newMessage: true
+          };
+        } else {
+          return {
+            ...state,
+            myChatroomMessages: chatroomMessages,
+            newMessage: null
+          };
         }
       }
-
-      return {
-        ...state,
-        myChatroomMessages: chatroomMessages,
-        newMessage: null
-      };
 
     default:
       return state;
